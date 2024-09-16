8c7fe408ae938401227c753b1cbdcf42dc603cf3
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2024-09-16 03:01:30 +0100

    graphics/linedrawer: create `graphics` module and implement `linedrawer`
    Create `linedrawer` function that draws a line on the framebuffer based
    on the line's orientation.

---
+++ src/graphics/linedrawer.rs
@@ -0,0 +1,64 @@
+use crate::core::framebuffer::FrameBuffer;
+
+/// Draws a line on the framebuffer where the X axis is the longer axis.
+fn draw_x_line(
+    fb: &mut FrameBuffer,
+    sx: i32, // Start X coordinate.
+    sy: i32, // Start Y coordinate.
+    ex: i32, // End X coordinate.
+    ey: i32, // End Y coordinate.
+) -> Result<(), &'static str> {
+    let dir = if sx <= ex { 1 } else { -1 };
+
+    let mut x = sx;
+    let mut y = sy as f32;
+    let slope = dir as f32 * (ey as f32 - sy as f32) / (ex as f32 - sx as f32);
+
+    while x != ex {
+        fb.plot_pixel(x as usize, y as usize, 1.0, 1.0, 1.0)?;
+        y += slope;
+        x += dir;
+    }
+
+    Ok(())
+}
+
+/// Draws a line on the framebuffer where the Y axis is the longer axis.
+fn draw_y_line(
+    fb: &mut FrameBuffer,
+    sx: i32, // Start X coordinate.
+    sy: i32, // Start Y coordinate.
+    ex: i32, // End X coordinate.
+    ey: i32, // End Y coordinate.
+) -> Result<(), &'static str> {
+    let dir = if sy <= ey { 1 } else { -1 };
+
+    let mut y = sy;
+    let mut x = sx as f32;
+    let slope = dir as f32 * (ex as f32 - sx as f32) / (ey as f32 - sy as f32);
+
+    while y != ey {
+        fb.plot_pixel(x as usize, y as usize, 1.0, 1.0, 1.0)?;
+        x += slope;
+        y += dir;
+    }
+
+    Ok(())
+}
+
+/// Draws a line on the framebuffer based on the line's orientation.
+pub fn draw_line(
+    fb: &mut FrameBuffer,
+    sx: i32, // Start X coordinate.
+    sy: i32, // Start Y coordinate.
+    ex: i32, // End X coordinate.
+    ey: i32, // End Y coordinate.
+) -> Result<(), &'static str> {
+    if sx == ex && sy == ey {
+        fb.plot_pixel(sx as usize, sy as usize, 1.0, 1.0, 1.0)
+    } else if (ex - sx).pow(2) >= (ey - sy).pow(2) {
+        draw_x_line(fb, sx, sy, ex, ey)
+    } else {
+        draw_y_line(fb, sx, sy, ex, ey)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 mod core;
+mod graphics;
 
 use core::framebuffer::FrameBuffer;
-use std::io::{Error, ErrorKind, Result};
+use graphics::linedrawer::draw_line;
+use std::{
+    error::Error,
+    f32::consts::PI,
+    io::{self, ErrorKind},
+    result::Result,
+};
 
-fn main() -> Result<()> {
-    let fb = FrameBuffer::new(512, 512).map_err(|err| Error::new(ErrorKind::Other, err))?;
+fn main() -> Result<(), Box<dyn Error>> {
+    // Create a framebuffer
+    let mut fb = FrameBuffer::new(512, 512)
+        .map_err(|err| Box::new(io::Error::new(ErrorKind::Other, err)))?;
+
+    for i in (0..64).map(|i| i as f32 * PI / 32.0) {
+        let x = (i.cos() * 48.0) as i32;
+        let y = (i.sin() * 48.0) as i32;
+        let ex = (i.cos() * 240.0) as i32;
+        let ey = (i.sin() * 240.0) as i32;
+
+        let _ = draw_line(&mut fb, 256 + x, 256 + y, 256 + ex, 256 + ey);
+    }
 
     fb.write_rgb_file("test.ppm")?;
 
