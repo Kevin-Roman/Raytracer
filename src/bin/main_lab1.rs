32f2032d1c43d652e7322c856b22984b7a800573
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2024-10-04 19:36:48 +0100

    graphics/linedrawer: use Bresenhams Line Algorithm for lab1
    Use Bresenhams Line Algorithm in order to complete lab1. Also, replace
    `sx, ex, sy, ey` syntax with `x0, y0, x1, y1` syntax for improved
    clarity.

--- src/bin/main_lab1.rs
+++ src/bin/main_lab1.rs
@@ -12,12 +12,12 @@ fn main() {
     };
 
     for i in (0..64).map(|i| i as f32 * PI / 32.0) {
-        let sx = 256 + (i.cos() * 48.0) as i32;
-        let sy = 256 + (i.sin() * 48.0) as i32;
-        let ex = 256 + (i.cos() * 240.0) as i32;
-        let ey = 256 + (i.sin() * 240.0) as i32;
+        let x0 = 256 + (i.cos() * 48.0) as i32;
+        let y0 = 256 + (i.sin() * 48.0) as i32;
+        let x1 = 256 + (i.cos() * 240.0) as i32;
+        let y1 = 256 + (i.sin() * 240.0) as i32;
 
-        let _ = draw_line(&mut fb, sx, sy, ex, ey);
+        let _ = draw_line(&mut fb, x0, y0, x1, y1);
     }
 
     if let Err(e) = fb.write_rgb_file("./output/lab1.ppm") {
--- src/graphics/linedrawer.rs
+++ src/graphics/linedrawer.rs
@@ -3,21 +3,28 @@ use crate::core::framebuffer::{FrameBuffer, FrameBufferError};
 /// Draws a line on the framebuffer where the X axis is the longer axis.
 fn draw_x_line(
     fb: &mut FrameBuffer,
-    sx: i32, // Start X coordinate.
-    sy: i32, // Start Y coordinate.
-    ex: i32, // End X coordinate.
-    ey: i32, // End Y coordinate.
+    x0: i32,
+    y0: i32,
+    x1: i32,
+    y1: i32,
 ) -> Result<(), FrameBufferError> {
-    let dir = if sx <= ex { 1 } else { -1 };
+    let direction = if y0 <= y1 { 1 } else { -1 };
+    let dx = x1 - x0;
+    let dy = direction * (y1 - y0);
 
-    let mut x = sx;
-    let mut y = sy as f32;
-    let slope = dir as f32 * (ey as f32 - sy as f32) / (ex as f32 - sx as f32);
+    // Difference parameter.
+    let mut p = 2 * dy - dx;
+    let mut y = y0;
 
-    while x != ex {
-        fb.plot_pixel(x, y as i32, 1.0, 1.0, 1.0)?;
-        y += slope;
-        x += dir;
+    for x in x0..x1 {
+        fb.plot_pixel(x, y, 1.0, 1.0, 1.0)?;
+
+        if p > 0 {
+            y += direction;
+            p += 2 * (dy - dx);
+        } else {
+            p += 2 * dy;
+        }
     }
 
     Ok(())
@@ -26,21 +33,28 @@ fn draw_x_line(
 /// Draws a line on the framebuffer where the Y axis is the longer axis.
 fn draw_y_line(
     fb: &mut FrameBuffer,
-    sx: i32, // Start X coordinate.
-    sy: i32, // Start Y coordinate.
-    ex: i32, // End X coordinate.
-    ey: i32, // End Y coordinate.
+    x0: i32,
+    y0: i32,
+    x1: i32,
+    y1: i32,
 ) -> Result<(), FrameBufferError> {
-    let dir = if sy <= ey { 1 } else { -1 };
+    let direction = if x0 <= x1 { 1 } else { -1 };
+    let dx = direction * (x1 - x0);
+    let dy = y1 - y0;
 
-    let mut y = sy;
-    let mut x = sx as f32;
-    let slope = dir as f32 * (ex as f32 - sx as f32) / (ey as f32 - sy as f32);
+    // Difference parameter.
+    let mut p = 2 * dx - dy;
+    let mut x = x0;
 
-    while y != ey {
-        fb.plot_pixel(x as i32, y, 1.0, 1.0, 1.0)?;
-        x += slope;
-        y += dir;
+    for y in y0..y1 {
+        fb.plot_pixel(x, y, 1.0, 1.0, 1.0)?;
+
+        if p > 0 {
+            x += direction;
+            p += 2 * (dx - dy);
+        } else {
+            p += 2 * dx;
+        }
     }
 
     Ok(())
@@ -49,16 +63,22 @@ fn draw_y_line(
 /// Draws a line on the framebuffer based on the line's orientation.
 pub fn draw_line(
     fb: &mut FrameBuffer,
-    sx: i32, // Start X coordinate.
-    sy: i32, // Start Y coordinate.
-    ex: i32, // End X coordinate.
-    ey: i32, // End Y coordinate.
+    x0: i32,
+    y0: i32,
+    x1: i32,
+    y1: i32,
 ) -> Result<(), FrameBufferError> {
-    if sx == ex && sy == ey {
-        fb.plot_pixel(sx, sy, 1.0, 1.0, 1.0)
-    } else if (ex - sx).pow(2) >= (ey - sy).pow(2) {
-        draw_x_line(fb, sx, sy, ex, ey)
+    if (y1 - y0).abs() < (x1 - x0).abs() {
+        if x0 > x1 {
+            draw_x_line(fb, x1, y1, x0, y0)
+        } else {
+            draw_x_line(fb, x0, y0, x1, y1)
+        }
     } else {
-        draw_y_line(fb, sx, sy, ex, ey)
+        if y0 > y1 {
+            draw_y_line(fb, x1, y1, x0, y0)
+        } else {
+            draw_y_line(fb, x0, y0, x1, y1)
+        }
     }
 }
