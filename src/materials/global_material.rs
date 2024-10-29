d823450340a97575a46c7fdbd75766163be66c50
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2024-10-29 23:21:39 +0000

    materials/global_material: create material for reflection and refraction
    The material is passed the environment which allows it to recursively
    raytrace reflections.

---
+++ src/materials/global_material.rs
@@ -0,0 +1,46 @@
+// The global material generates a reflection/refraction layer.
+
+use crate::core::{
+    colour::Colour, environment::Environment, hit::Hit, material::Material, ray::Ray,
+    scene::SMALL_ROUNDING_ERROR, vector::Vector,
+};
+
+pub struct GlobalMaterial {
+    reflect_weight: Colour,
+    refract_weight: Colour,
+    index_of_refraction: f32,
+}
+
+impl GlobalMaterial {
+    pub fn new(reflect_weight: Colour, refract_weight: Colour, index_of_refraction: f32) -> Self {
+        Self {
+            reflect_weight,
+            refract_weight,
+            index_of_refraction,
+        }
+    }
+}
+
+impl Material for GlobalMaterial {
+    fn compute_once(
+        &self,
+        environment: &mut dyn Environment,
+        viewer: &Ray,
+        hit: &Hit,
+        recurse: i32,
+    ) -> Colour {
+        if recurse == 0 {
+            return Colour::default();
+        }
+
+        let mut reflection_ray = Ray::default();
+        reflection_ray.direction = viewer.direction.negate().reflection(&hit.normal);
+        reflection_ray.position = hit.position + SMALL_ROUNDING_ERROR * reflection_ray.direction;
+
+        self.reflect_weight * environment.raytrace(&reflection_ray, recurse - 1).0
+    }
+
+    fn compute_per_light(&self, _viewer: &Vector, _light_direction: &Vector, _hit: &Hit) -> Colour {
+        Colour::default()
+    }
+}
--- src/materials/mod.rs
+++ src/materials/mod.rs
@@ -1,2 +1,3 @@
 pub mod falsecolour_material;
+pub mod global_material;
 pub mod phong_material;
